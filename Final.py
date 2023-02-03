18cbcf9a46b19f4cee9ce8f8ad1d2d47ee5e5a18
Tharit Sinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2023-02-03 18:02:06 +0900

    single stifness 1

--- Final.py
+++ Final.py
@@ -11,26 +11,51 @@ N3 = [1,1]
 
 M = [N1, N2, N3]
 
-def mkB(M):
-    B = np.array([[M[1][1]-M[2][1],        0       , M[2][1]-M[0][1],        0       , M[0][1]-M[1][1],        0       ], \
-                  [       0       , M[2][0]-M[1][0],        0       , M[0][0]-M[2][0],        0       , M[1][0]-M[0][0]], \
-                  [M[2][0]-M[1][0], M[1][1]-M[2][1], M[0][0]-M[2][0], M[2][1]-M[0][1], M[1][0]-M[0][0], M[0][1]-M[1][1]]])
-    return B
+class K_pstrain:
+    def __init__(self, M, E, v):
+        self.E = E
+        self.v = v
+        self.M = M
+        self.x1 = M[0][0]
+        self.y1 = M[0][1]
+        self.x2 = M[1][0]
+        self.y2 = M[1][1]
+        self.x3 = M[2][0]
+        self.y3 = M[2][1]
+    
+    def B(self):
+        B = np.array([[self.M[1][1]-self.M[2][1],             0            , self.M[2][1]-self.M[0][1],             0            , self.M[0][1]-self.M[1][1],             0            ], \
+                      [            0            , self.M[2][0]-self.M[1][0],             0            , self.M[0][0]-self.M[2][0],             0            , self.M[1][0]-self.M[0][0]], \
+                      [self.M[2][0]-self.M[1][0], self.M[1][1]-self.M[2][1], self.M[0][0]-self.M[2][0], self.M[2][1]-self.M[0][1], self.M[1][0]-self.M[0][0], self.M[0][1]-self.M[1][1]]])
+        # B = np.array([[M[1][1]-M[2][1],        0       , M[2][1]-M[0][1],        0       , M[0][1]-M[1][1],        0       ], \
+        #             [       0       , M[2][0]-M[1][0],        0       , M[0][0]-M[2][0],        0       , M[1][0]-M[0][0]], \
+        #             [M[2][0]-M[1][0], M[1][1]-M[2][1], M[0][0]-M[2][0], M[2][1]-M[0][1], M[1][0]-M[0][0], M[0][1]-M[1][1]]])
+        return B
 
-def mkD_pstrain(E, v):
-    d = np.array([[1-v,  v ,     0    ], \
-                 [ v , 1-v,     0    ], \
-                 [ 0 ,  0 , (1-2*v)/2]])
+    def D(self):
+        d = np.array([[1-self.v,    self.v,        0      ], \
+                      [ self.v ,  1-self.v,        0      ], \
+                      [ 0      ,      0   , (1-2*self.v)/2]])
 
-    D = E / (1+v) / (1-2*v) * d
+        D = E / (1+v) / (1-2*v) * d
 
-    return D
+        return D
 
+    def mesharea(self):
+        a = 0.5 * np.linalg.det(np.array([[1,self.x1,self.y1], [1,self.x2,self.y2], [1,self.x3,self.y3]]))
+        return a
 
+    def K(self):
+        a = self.mesharea()
+        B = self.B()
+        D = self.D()
+        Bt = np.transpose(B)
 
+        k = a * Bt.dot(D.dot(B))
+        
+        return k
 
-B = mkB(M)
-D = mkD_pstrain(E,v)
+K = K_pstrain(M,E,v)
 
-print(B)
-print(D)
+print(K.K())
+
